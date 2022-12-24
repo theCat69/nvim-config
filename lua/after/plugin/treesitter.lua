34bd0eae12063ddce8c275aa0110f8e71745ae17
Felix Vadcard <vadcard.felix@gmail.com>
2022-12-24 17:31:09 +0100

    reactivating lua as highlighted with treesitter seems to work now

--- lua/after/plugin/treesitter.lua
+++ lua/after/plugin/treesitter.lua
@@ -6,7 +6,6 @@ require('nvim-treesitter.configs').setup {
 
   highlight = {
     enable = true,
-    disable = { 'lua' },
     additional_vim_regex_highlighting = false,
   },
   indent = { enable = true },
