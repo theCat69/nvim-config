a7e2af8a0d1357235a49bdb4640d119ff7de221a
Felix Vadcard <vadcard.felix@gmail.com>
2023-05-23 18:07:07 +0200

    nvim-tree remaps and refactoring nvim

--- init.lua
+++ init.lua
@@ -50,10 +50,10 @@ require('packer').startup(function(use)
   use 'tpope/vim-rhubarb'
   use 'lewis6991/gitsigns.nvim'
 
-  use 'navarasu/onedark.nvim' -- Theme inspired by Atom
-  use 'nvim-lualine/lualine.nvim' -- Fancier statusline
+  use 'navarasu/onedark.nvim'               -- Theme inspired by Atom
+  use 'nvim-lualine/lualine.nvim'           -- Fancier statusline
   use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
-  use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
+  use 'numToStr/Comment.nvim'               -- "gc" to comment visual regions/lines
 
   -- Fuzzy Finder (files, lsp, etc)
   use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
@@ -83,6 +83,14 @@ require('packer').startup(function(use)
     config = function()
       require("nvim-tree").setup {}
     end
+  }
+
+  use {
+    "ThePrimeagen/refactoring.nvim",
+    requires = {
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-treesitter/nvim-treesitter" }
+    }
   }
 
   if is_bootstrap then
