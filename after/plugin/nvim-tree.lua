d633fbb420f6080a939e06689cb2a068181762bd
Felix Vadcard <vadcard.felix@gmail.com>
2023-12-06 09:54:15 +0100

    nvim tree close on open file and enable diag

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -1,16 +1,27 @@
--- empty setup using defaults
-require("nvim-tree").setup()
--- require("nvim-tree").setup {
---   actions = {
---     open_file = {
---       quit_on_open = true,
---     },
---   },
---   diagnostics = {
---     enable = true,
---     show_on_dirs = true,
---   },
--- }
+local function my_on_attach(bufnr)
+  local api = require "nvim-tree.api"
 
-vim.keymap.set('n', '<leader>tt', '<Cmd>NvimTreeToggle<CR>', { silent = true })
-vim.keymap.set('n', '<leader>tf', '<Cmd>NvimTreeFindFile<CR>', { silent = true })
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  vim.keymap.set('n', '<leader>tt', '<Cmd>NvimTreeToggle<CR>', { silent = true })
+  vim.keymap.set('n', '<leader>tf', '<Cmd>NvimTreeFindFile<CR>', { silent = true })
+end
+
+require("nvim-tree").setup({
+  actions = {
+    open_file = {
+      quit_on_open = true,
+    },
+  },
+  diagnostics = {
+    enable = true,
+    show_on_dirs = true,
+  },
+  on_attach = my_on_attach,
+})
