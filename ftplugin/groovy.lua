64b695011ecedbcee366ad27806e935814032cb7
Felix Vadcard <vadcard.felix@gmail.com>
2023-12-13 11:09:49 +0100

    remobing auto format for groovy and nginx adding groovy and nginx treesitter

--- ftplugin/groovy.lua
+++ ftplugin/groovy.lua
@@ -1,9 +1,13 @@
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-
-require('lspconfig')['groovyls'].setup {
-  capabilities = capabilities,
-}
+-- local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+--
+-- require('lspconfig')['groovyls'].setup {
+--   capabilities = capabilities,
+--   cmd = { 'java', '-jar', os.getenv('NVIM_DATA') .. '/mason/packages/groovy-language-server/build/libs/groovy-language-server-all.jar' },
+--   filetypes = {
+--     "groovy",
+--   },
+-- }
 
 vim.keymap.set('n', '<A-l>', 'mj gg=G `j')
 
@@ -11,7 +15,7 @@ vim.api.nvim_create_autocmd('BufWritePre', {
   group = vim.api.nvim_create_augroup('onSaveEventGroovy', { clear = true }),
   pattern = { "Jenkinsfile", "*.groovy" }, -- supported format on save
   callback = function()
-    vim.api.nvim_input('<A-l>')
+    -- vim.api.nvim_input('<A-l>')
     require("jenkinsfile_linter").validate()
   end,
 })
