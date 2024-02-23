058282af0d02016285f46c7b4cbbea340805043a
Felix Vadcard <vadcard.felix@gmail.com>
2024-02-23 10:31:12 +0100

    adding autoinstall for treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -4,6 +4,8 @@ require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
   ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'java', 'yaml', 'kotlin', 'tsx', 'bash', 'groovy', 'zig' },
 
+  auto_install = true,
+
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
