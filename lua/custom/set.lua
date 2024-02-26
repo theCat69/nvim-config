7eafced270a77e526cff0a07e1506a0588659910
Felix Vadcard <vadcard.felix@gmail.com>
2024-02-26 13:01:59 +0100

    changing tabstop to 2 i dunno why it was 3

--- lua/custom/set.lua
+++ lua/custom/set.lua
@@ -24,7 +24,7 @@ vim.wo.number = true
 vim.o.mouse = 'a'
 
 -- defining tabstop and shiftwidth
-vim.opt.tabstop = 3
+vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.bo.softtabstop = 2
