715185dcfd73157e4c93198b26d05d1e03ce537f
Felix Vadcard <vadcard.felix@gmail.com>
2024-03-05 15:43:17 +0100

    forgetting to had friendly snippets

--- lua/plugins/no-lsp/cmp.lua
+++ lua/plugins/no-lsp/cmp.lua
@@ -51,5 +51,8 @@ return {
     'hrsh7th/nvim-cmp',
     dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
     config = config
+  },
+  {
+    'theCat69/friendly-snippets'
   }
 }
